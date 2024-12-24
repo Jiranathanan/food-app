595fd5ca4f4e463cd2d7b58897dd2ce4c0f5d6e9
Jiranathanan Siripun <jiranathanan@gmail.com>
2024-12-24 10:12:42 +0700

    Fix env API KEY problem

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,16 +2,33 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
 import { YELP_API_KEY } from '@env';
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        try {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses);
+        } catch (error) {
+        console.log(error);
+        }
+    }
 
     return <View >
-        <Text>{YELP_API_KEY}</Text>
+        <Text> API KEY IS: {YELP_API_KEY}</Text>
         <SearchBar 
             term={term} 
             onTermChange={newTerm => setTerm(newTerm)} 
-            onTermSubmit={() => console.log('term was submitted')}
+            onTermSubmit={searchApi}
             />
         <Text style={{ marginLeft: 40}}>
                 {term.length > 0 
@@ -19,6 +36,7 @@ const SearchScreen = () => {
                     : null
                 }
         </Text>
+        <Text style={{ marginLeft: 20 }}>We have found {results.length} results</Text>
     </View>
 }
 
