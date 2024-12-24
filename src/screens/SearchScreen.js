b2585258aa353146869ed2daf00749c69f7e95f6
Jiranathanan Siripun <jiranathanan@gmail.com>
2024-12-25 04:30:59 +0700

    Use useEffect to set pre-search function to run only once

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
-import { YELP_API_KEY } from '@env';
+// import { YELP_API_KEY } from '@env';
 import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
         try {
         const response = await yelp.get('/search', {
             params: {
                 limit: 50,
-                term,
+                term: searchTerm,
                 location: 'san jose'
             }
         });
         setResults(response.data.businesses);
         } catch (error) {
-        console.log(error);
+            setErrorMessage('Ops ! Something went wrong');
         }
     }
 
+    useEffect( () => {
+        searchApi('pasta');
+        console.log('Searching...')
+    }, [])
+
     return <View >
-        <Text> API KEY IS: {YELP_API_KEY}</Text>
+        {/* <Text> API KEY IS: {YELP_API_KEY}</Text> */}
         <SearchBar 
             term={term} 
             onTermChange={newTerm => setTerm(newTerm)} 
-            onTermSubmit={searchApi}
+            onTermSubmit={() => searchApi(term)}
             />
         <Text style={{ marginLeft: 40}}>
                 {term.length > 0 
@@ -36,7 +42,8 @@ const SearchScreen = () => {
                     : null
                 }
         </Text>
-        <Text style={{ marginLeft: 20 }}>We have found {results.length} results</Text>
+        {errorMessage ? <Text style={{color: 'purple', marginLeft: 20}}>{errorMessage}</Text> : null}
+        {results.length > 0 ? <Text style={{ marginLeft: 40 }}>We have found {results.length} results</Text> : null}
     </View>
 }
 
